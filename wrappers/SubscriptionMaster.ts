3e97de164601fa1db919eecda132d0a879798976
yuriarthf <yuriarthf@protonmail.com>
2023-06-11 17:02:06 +0700

    feat[WIP]: subscriptionMasterConfigToCell and Opcodes

--- wrappers/SubscriptionMaster.ts
+++ wrappers/SubscriptionMaster.ts
@@ -1,16 +1,74 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import {
+    Address,
+    Dictionary,
+    beginCell,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider,
+    Sender,
+    SendMode,
+} from 'ton-core';
+
+import { sha256_sync } from "ton-crypto";
+
 
 export type SubscriptionMasterConfig = {
-    id: number;
-    counter: number;
+    metadata: {
+        name?: string;
+        description?: string;
+    };
 };
 
+function toSnakeFormat(str: string): Cell {
+    const snakeCell = beginCell();
+    if (str.length > 127) { // 127 bytes = 1016 bits (< 1023)
+        snakeCell.storeStringTail(str.slice(0, 127));
+        snakeCell.storeRef(toSnakeFormat(str.slice(127)));
+    } else {
+        snakeCell.storeStringTail(str);
+    }
+
+    return snakeCell.endCell();
+}
+
 export function subscriptionMasterConfigToCell(config: SubscriptionMasterConfig): Cell {
-    return beginCell().storeUint(config.id, 32).storeUint(config.counter, 32).endCell();
+    const metadata = Dictionary.empty(
+        Dictionary.Keys.Buffer(32),
+        Dictionary.Values.Cell()
+    );
+
+    if (config.metadata.name) {
+        metadata.set(
+            sha256_sync("name"),
+            beginCell()
+                .storeUint(0, 8) // Snake format data
+                .storeRef(toSnakeFormat(config.metadata.name))
+            .endCell()
+        );
+    }
+
+    if (config.metadata.description) {
+        metadata.set(
+            sha256_sync("description"),
+            beginCell()
+                .storeUint(0, 8) // Snake format data
+                .storeRef(toSnakeFormat(config.metadata.description))
+            .endCell()
+        );
+    }
+
+    return beginCell()
+        .storeUint(0, 8) // onchain data
+        .storeDict(metadata)
+    .endCell();
 }
 
 export const Opcodes = {
-    increase: 0x7e8764ef,
+    subscribe: 0x5fcc3d14 & 0x7fffffff,
+    configure: 0x9e90e363 & 0x7fffffff,
+    change_manager: 0x6780b0d9 & 0x7fffffff,
+    update_subscription_authority: 0x944304c1 & 0x7fffffff
 };
 
 export class SubscriptionMaster implements Contract {
