b6c51f737ef115b5708522cfe670321786a414e6
yuriarthf <yuriarthf@protonmail.com>
2023-07-06 20:36:13 +0700

    feat: Add owner as WalletV4 to use plugins

--- tests/Subscription.spec.ts
+++ tests/Subscription.spec.ts
@@ -1,5 +1,12 @@
-import { Blockchain, SandboxContract, TreasuryContract } from "@ton-community/sandbox";
-import { Cell, beginCell, toNano } from "ton-core";
+import {
+    Blockchain,
+    SandboxContract,
+    TreasuryContract,
+    
+} from "@ton-community/sandbox";
+import { Cell, beginCell, toNano, SendMode } from "ton-core";
+import { mnemonicNew, mnemonicToPrivateKey } from "ton-crypto"
+import { WalletContractV4 } from "ton";
 import { Subscription } from "../wrappers/Subscription";
 import "@ton-community/test-utils";
 import { compile } from "@ton-community/blueprint";
@@ -10,7 +17,7 @@ import { compile } from "@ton-community/blueprint";
 
 describe("Subscription", () => {
     let subscriptionMasterMock: SandboxContract<TreasuryContract>;
-    let owner: SandboxContract<TreasuryContract>;
+    let owner: SandboxContract<WalletContractV4>;
     let manager: SandboxContract<TreasuryContract>;
     let blockchain: Blockchain;
     let subscriptionCode: Cell;
@@ -20,15 +27,31 @@ describe("Subscription", () => {
     const FEE = toNano("5");
     const PERIOD = 2630000n;
 
+    const WORKCHAIN_ID = 0;
     const MIN_TON_RESERVE = 50000000n;
 
     beforeAll(async () => {
         blockchain = await Blockchain.create();
 
-
         subscriptionMasterMock = await blockchain.treasury("subscriptionMasterMock");
-        owner = await blockchain.treasury("owner");
         manager = await blockchain.treasury("manager");
+
+        const ownerDonator = await blockchain.treasury("ownerDonator");
+
+        const mnemonic = await mnemonicNew();
+        const keyPair = await mnemonicToPrivateKey(mnemonic);
+        owner = blockchain.openContract(WalletContractV4.create({
+            workchain: WORKCHAIN_ID,
+            publicKey: keyPair.publicKey
+        }));
+
+        ownerDonator.send({
+            value: 0n,
+            to: owner.address,
+            sendMode: SendMode.CARRY_ALL_REMAINING_BALANCE 
+                + SendMode.DESTROY_ACCOUNT_IF_ZERO,
+            bounce: false,
+        });
 
         subscriptionCode = await compile('Subscription');
 
@@ -37,7 +60,7 @@ describe("Subscription", () => {
                 subscriptionMasterMock.address,
                 owner.address,
                 subscriptionCode,
-                0
+                
             )
         );
     });
@@ -75,5 +98,9 @@ describe("Subscription", () => {
         expect(subscriptionMetadata.fee).toEqual(FEE);
         expect(subscriptionMetadata.period).toEqual(PERIOD);
         expect(subscriptionMetadata.activated).toEqual(false);
+    });
+
+    it("op::activate", async () => {
+        // Under construction
     });
 });
